b08e028e5136df64b8ff9210c44e2933d701e0d0
Blanxxty <Mvtvr21@yandex.ru>
2023-09-11 13:10:46 +0300

    Initial Commit

---
+++ blanxxtyTwoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  blanxxtyTwoApp
+//
+//  Created by Марат Хасанов on 11.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
